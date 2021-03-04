b81b7605a1dbdafbbb1ce45a73a4d923c74daf4b
Carey Willette <carey.willette@gmail.com>
2021-03-04 12:50:55 -0700

    remove console log

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 function requireHTTPS(req, res, next) {
     // The 'x-forwarded-proto' check is for Heroku
-    console.log('%c http request: ', 'background: #41ff6b; color: #ff4700;', req);
     if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
       return res.redirect('https://' + req.get('host') + req.url);
     }
