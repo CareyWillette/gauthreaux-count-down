79cebbdf69754ee1957835319ee9aa4fd739d552
Carey Willette <carey.willette@gmail.com>
2021-03-04 12:40:44 -0700

    video and timer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'gauthreaux-count-down';
+export class AppComponent implements OnInit, OnDestroy {
+  private subscription: Subscription;
+
+  public dateNow = new Date();
+  public dDay = new Date('Mar 09 2021 12:00:00');
+  milliSecondsInASecond = 1000;
+  hoursInADay = 24;
+  minutesInAnHour = 60;
+  SecondsInAMinute = 60;
+
+  public timeDifference;
+  public secondsToDday;
+  public minutesToDday;
+  public hoursToDday;
+  public daysToDday;
+
+  visible = false;
+
+  private getTimeDifference() {
+    this.timeDifference = this.dDay.getTime() - new Date().getTime();
+    if (this.timeDifference > 0) {
+      this.allocateTimeUnits(this.timeDifference);
+    } else {
+      this.secondsToDday = 0;
+      this.minutesToDday = 0;
+      this.hoursToDday = 0;
+      this.daysToDday = 0;
+    }
+  }
+
+  private allocateTimeUnits(timeDifference) {
+    this.secondsToDday = Math.floor(
+      (timeDifference / this.milliSecondsInASecond) % this.SecondsInAMinute
+    );
+    this.minutesToDday = Math.floor(
+      (timeDifference / (this.milliSecondsInASecond * this.minutesInAnHour)) %
+        this.SecondsInAMinute
+    );
+    this.hoursToDday = Math.floor(
+      (timeDifference /
+        (this.milliSecondsInASecond *
+          this.minutesInAnHour *
+          this.SecondsInAMinute)) %
+        this.hoursInADay
+    );
+    this.daysToDday = Math.floor(
+      timeDifference /
+        (this.milliSecondsInASecond *
+          this.minutesInAnHour *
+          this.SecondsInAMinute *
+          this.hoursInADay)
+    );
+  }
+
+  ngOnInit() {
+    this.subscription = interval(1000).subscribe((x) => {
+      this.getTimeDifference();
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
